9ebad41fbfaa9ab39acfec3d4f3bd91e591c078b
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-07-12 14:18:18 -0500

    server boilerplate

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = 5000;
+
+app.use(express.static('server/public'));
+
+
+
+http.listen(PORT, err => {
+  if (err) {
+    console.log(`Error listening on port ${PORT}`,err);
+  } else {
+    console.log(`Listening on port ${PORT}...`);
+  }
+})
