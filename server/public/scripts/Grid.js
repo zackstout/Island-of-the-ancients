5cd9f6ee1485a8907e8e1732c1613e51949d27f6
Zack Stout <zackstout@gmail.com>
2018-07-12 15:16:41 -0500

    random tiling of colors showing up

--- server/public/scripts/Grid.js
+++ server/public/scripts/Grid.js
@@ -1,7 +1,29 @@
 
-export function Grid(h, w) {
-  this.h = h;
-  this.w = w;
+const resources = ["stone", "iron"];
+
+export function Grid(h, w, numCellsH, numCellsW) {
+
+  this.numCellsH = numCellsH;
+  this.numCellsW = numCellsW;
+
+  this.cell_height = h / numCellsH;
+  this.cell_width = w / numCellsW;
+
+  this.generateSquares = function() {
+    let squares = [];
+    for (let i=0; i < this.numCellsW; i++) {
+      for (let j=0; j < this.numCellsH; j++) {
+        let resource = Math.random() > 0.5 ? resources[0] : resources[1];
+        let square = {x: i, y: j, resource: resource};
+
+        squares.push(square);
+      }
+    }
+    this.squares = squares;
+    // return squares;
+  };
+
+  this.generateSquares();
 }
 
 // module.exports = Grid;
--- server/public/scripts/draw.js
+++ server/public/scripts/draw.js
@@ -1,19 +1,33 @@
 
-let height, width;
-// import { Grid } from './Grid.js';
-console.log('what up');
+let height, width, ctx;
+import { Grid } from './Grid.js';
 
 
 
 window.onload = function() {
   height = 500;
   width = 500;
-  let ctx = document.getElementById('island').getContext('2d');
+  ctx = document.getElementById('island').getContext('2d');
   ctx.fillStyle = 'blue';
   ctx.fillRect(0, 0, width, height);
+
+
+  let grid = new Grid(height, width, 12, 12);
+  console.log(grid);
+  drawGrid(grid);
+
+
 };
 
 
-
-  // let grid = new Grid(1, 2);
-  // console.log(grid);
+function drawGrid(grid) {
+  grid.squares.forEach(square => {
+    let col;
+    switch(square.resource) {
+      case 'stone': col = 'gray'; break;
+      case 'iron': col = 'brown'; break;
+    }
+    ctx.fillStyle = col;
+    ctx.fillRect(square.x * grid.cell_height, square.y * grid.cell_width, grid.cell_width, grid.cell_height);
+  });
+}
