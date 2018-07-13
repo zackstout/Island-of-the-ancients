e8fda152cbc6335d10ed505e1ba0454b6238b2ec
Zack Stout <zackstout@gmail.com>
2018-07-12 20:55:44 -0500

    determining nearest vertext to a click

--- server/public/scripts/Grid.js
+++ server/public/scripts/Grid.js
@@ -1,21 +1,32 @@
 
-const resources = ["stone", "iron"];
+const resources = ["stone", "iron", "gem"];
 
 import { Cell } from './Cell.js';
 
 export function Grid(h, w, numCellsH, numCellsW) {
-
   this.numCellsH = numCellsH;
   this.numCellsW = numCellsW;
-
   this.cell_height = h / numCellsH;
   this.cell_width = w / numCellsW;
+  this.cells = [];
 
   this.generateCells = function() {
     let cells = [];
     for (let i=0; i < this.numCellsW; i++) {
       for (let j=0; j < this.numCellsH; j++) {
-        let resource = Math.random() > 0.5 ? resources[0] : resources[1];
+        const rarity = 27;
+        let random_int = Math.floor(Math.random() * rarity);
+        let resource;
+
+        if (random_int === 0) {
+          resource = resources[2];
+        } else if (random_int < Math.ceil(rarity/2)) {
+          resource = resources[0];
+        } else {
+          resource = resources[1];
+        }
+
+        // let resource = Math.random() > 0.5 ? resources[0] : resources[1];
         let cell = new Cell(i, j, resource);
         cell.vertices = cell.computeVertices(); // Not sure this is needed
 
--- server/public/scripts/draw.js
+++ server/public/scripts/draw.js
@@ -1,10 +1,11 @@
 
+// It's clearly not going to work to make grid a global var:
 let height, width, ctx, grid;
 import { Grid } from './Grid.js';
 
 
 let test_vertices = [{x: 10, y: 3, occupant: 'P1'}, {x: 2, y: 4, occupant: 'P1'}, {x: 3, y:3, occupant: 'P2'}, {x: 2, y: 1, occupant: 'P1'}];
-let test_edges = [[{x:1, y:2}, {x:2,y:2}], [{x:1, y:3}, {x:1, y:4}]];
+let test_edges = [[{x:1, y:2}, {x:2, y:2}], [{x:1, y:3}, {x:1, y:4}]];
 
 
 window.onload = function() {
@@ -22,6 +23,9 @@ window.onload = function() {
   canvas.addEventListener("click", handleClick);
 };
 
+function getDistance(a, b) {
+  return Math.pow(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2), 0.5);
+}
 
 function handleClick(e) {
   // NOTE: Will only work when canvas butts up against edge of browser:
@@ -30,17 +34,42 @@ function handleClick(e) {
   let cell_x = Math.floor(mouse.x / grid.cell_width);
   let cell_y = Math.floor(mouse.y / grid.cell_height);
 
-  console.log(grid.findCell(cell_x, cell_y));
-  return grid.findCell(cell_x, cell_y);
+  let cell = grid.findCell(cell_x, cell_y);
+
+
+  // Get nearest vertex to clicked point:
+  let vertices = cell.computeVertices().map(v => {
+    let res = {x: v.x * grid.cell_width, y: v.y * grid.cell_height};
+    return res;
+  });
+
+  let minDist = 1000;
+  let closestVertex = {};
+
+  vertices.forEach(v => {
+    let d = getDistance(mouse, v);
+    if (d < minDist) {
+      minDist = d;
+      closestVertex.x = Math.round(v.x / grid.cell_width);
+      closestVertex.y = Math.round(v.y / grid.cell_height);
+    }
+  });
+
+  console.log(cell, closestVertex);
+
+
+
+  return cell;
 }
 
-
+// I want to put these inside the Grid constructor -- but can ctx be easily accessed from there?
 function drawGrid(grid, occupied_vertices=[], occupied_edges=[]) {
   grid.cells.forEach(cell => {
     let col;
     switch(cell.resource) {
       case 'stone': col = 'gray'; break;
       case 'iron': col = 'brown'; break;
+      case 'gem': col = 'plum'; break;
     }
     ctx.fillStyle = col;
     ctx.fillRect(cell.x * grid.cell_width, cell.y * grid.cell_height, grid.cell_width, grid.cell_height);
